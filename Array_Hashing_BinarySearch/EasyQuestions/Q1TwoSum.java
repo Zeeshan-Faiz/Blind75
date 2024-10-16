f226e561fedb2e96be8e33866c9accfc805fc8ea
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-10-16 19:47:53 +0530

    Added Q1TwoSum java

--- Array_Hashing_BinarySearch/EasyQuestions/Q1TwoSum.java
+++ Array_Hashing_BinarySearch/EasyQuestions/Q1TwoSum.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /*
 Given an array of integers nums and an integer target, return indices of the two numbers 
 such that they add up to target.
@@ -21,4 +24,15 @@ Output: [0,1]
 
 public class Q1TwoSum{
 
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(target - nums[i]))
+                return new int[] {map.get(target - nums[i]), i};
+            
+            //or else add the current value in map
+            map.put(nums[i],i);
+        }
+        return new int[] {};
+    }
 }
