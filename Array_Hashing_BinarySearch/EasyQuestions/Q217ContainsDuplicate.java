879be844c2f920c949cc59b19be7e7f9c155e489
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-10-18 16:41:42 +0530

    Added Q217ContainsDuplicate code

--- Array_Hashing_BinarySearch/EasyQuestions/Q217ContainsDuplicate.java
+++ Array_Hashing_BinarySearch/EasyQuestions/Q217ContainsDuplicate.java
@@ -1,3 +1,12 @@
+
+/*
+Given an integer array nums, return true if any value appears at least twice in the array, and 
+return false if every element is distinct.
+
+Example 1:
+
+*/
+
 public class Q217ContainsDuplicate {
     
 }
