b5d33202a590422d55e4775a71c66f003544f085
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-10-18 16:42:16 +0530

    Added Q217ContainsDuplicate code

--- Array_Hashing_BinarySearch/EasyQuestions/Q217ContainsDuplicate.java
+++ Array_Hashing_BinarySearch/EasyQuestions/Q217ContainsDuplicate.java
@@ -20,6 +20,17 @@ Input: nums = [1,1,1,3,3,4,3,2,4,2]
 Output: true
 */
 
+import java.util.HashSet;
+
 public class Q217ContainsDuplicate {
-    
+
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (!set.add(num)) {
+                return true; // If the element is already present in the set, it's a duplicate
+            }
+        }
+        return false; // No duplicates
+    }
 }
