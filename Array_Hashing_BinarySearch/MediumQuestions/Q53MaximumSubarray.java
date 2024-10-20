350a41ac74ec93509f227d765ceaa5f073a8e578
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-10-20 15:20:38 +0530

    Added Q53MaximumSubarray code

--- Array_Hashing_BinarySearch/MediumQuestions/Q53MaximumSubarray.java
+++ Array_Hashing_BinarySearch/MediumQuestions/Q53MaximumSubarray.java
@@ -1,5 +1,12 @@
 package Array_Hashing_BinarySearch.MediumQuestions;
 
+/*
+Given an integer array nums, find the subarray with the largest sum, and return its sum.
+
+Example 1:
+
+*/
+
 public class Q53MaximumSubarray {
     
 }
