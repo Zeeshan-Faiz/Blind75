6b24cefc71002fca7ed00983c4179f23e589451a
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-10-21 16:24:10 +0530

    Added Q152MaximumProductSubArray code

--- Array_Hashing_BinarySearch/MediumQuestions/Q152MaximumProductSubArray.java
+++ Array_Hashing_BinarySearch/MediumQuestions/Q152MaximumProductSubArray.java
@@ -1,5 +1,13 @@
 package Array_Hashing_BinarySearch.MediumQuestions;
 
+/*
+Given an integer array nums, find a subarray that has the largest product, and return the product.
+The test cases are generated so that the answer will fit in a 32-bit integer.
+
+Example 1:
+
+*/
+
 public class Q152MaximumProductSubArray {
     
 }
