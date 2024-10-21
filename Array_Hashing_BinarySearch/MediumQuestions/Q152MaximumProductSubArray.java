5a31cea4e781a8d89303a800702d7bc17d181dd8
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-10-21 16:24:35 +0530

    Added Q152MaximumProductSubArray code

--- Array_Hashing_BinarySearch/MediumQuestions/Q152MaximumProductSubArray.java
+++ Array_Hashing_BinarySearch/MediumQuestions/Q152MaximumProductSubArray.java
@@ -17,4 +17,29 @@ Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
 
 public class Q152MaximumProductSubArray {
     
+    public int maxProduct(int[] nums) {
+        
+        if (nums == null || nums.length == 0)
+            return 0;
+
+        int n = nums.length;
+        long leftProd = 1, rightProd = 1;
+        long maxProd = nums[0];
+
+        for (int i = 0; i < n; i++) {
+
+            // if any of leftProd or rightProd become 0 then update it to 1
+            leftProd = (leftProd == 0 || leftProd < Integer.MIN_VALUE) ? 1 : leftProd;
+            rightProd = (rightProd == 0 || rightProd < Integer.MIN_VALUE) ? 1 : rightProd;
+
+            // prefix product
+            leftProd = leftProd * nums[i];
+
+            // suffix product
+            rightProd = rightProd * nums[n - 1 - i];
+
+            maxProd = Math.max(maxProd, Math.max(leftProd, rightProd));
+        }
+        return (int) maxProd;
+    }
 }
