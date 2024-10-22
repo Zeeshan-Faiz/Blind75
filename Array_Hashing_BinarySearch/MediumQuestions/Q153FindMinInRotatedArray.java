619dd910cfd7a9b240a37bb5c5262fdd3ce84b4b
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-10-22 18:42:12 +0530

    Added Q153FindMinInRotatedArray code

--- Array_Hashing_BinarySearch/MediumQuestions/Q153FindMinInRotatedArray.java
+++ Array_Hashing_BinarySearch/MediumQuestions/Q153FindMinInRotatedArray.java
@@ -27,6 +27,25 @@ Output: 11
 Explanation: The original array was [11,13,15,17] and it was rotated 4 times. 
 */
 
-public class Q153FindMinInRotatedArray{
-    
+public class Q153FindMinInRotatedArray {
+
+    public int findMin(int[] nums) {
+
+        int st = 0, end = nums.length - 1;
+        int min = nums[0];
+        while (st <= end) {
+
+            int mid = (st + end) / 2;
+            if (min <= nums[mid]) { // emliminate left sorted part
+                st = mid + 1;
+            } 
+            else {
+                if (nums[mid] < min) {
+                    min = nums[mid];
+                }
+                end = mid - 1;
+            }
+        }
+        return min;
+    }
 }
