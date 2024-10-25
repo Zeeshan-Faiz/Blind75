0f4e3b026c77a689779333386b0a091bf3160f3a
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-10-25 16:24:48 +0530

    Added Q11ContainerWithMostWater code Added  code

--- Array_Hashing_BinarySearch/MediumQuestions/Q11ContainerWithMostWater.java
+++ Array_Hashing_BinarySearch/MediumQuestions/Q11ContainerWithMostWater.java
@@ -20,4 +20,23 @@ Output: 1
 
 public class Q11ContainerWithMostWater {
     
+    public int maxArea(int[] height) {
+        
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+        while (left < right) 
+        {
+            int currentArea = Math.min(height[left], height[right]) * (right - left);
+            maxArea = Math.max(maxArea, currentArea);
+
+            if (height[left] < height[right]) {
+                left++;
+            } 
+            else {
+                right--;
+            }
+        }
+        return maxArea;
+    }
 }
