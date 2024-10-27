70a389d33a8760399d190f6b27ce1404bfaf69d6
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-10-27 20:17:42 +0530

    Added Q191NumberOfOneBits code

--- BitManupulation/Q191NumberOfOneBits.java
+++ BitManupulation/Q191NumberOfOneBits.java
@@ -1,5 +1,13 @@
 package BitManupulation;
 
+/*
+Given a positive integer n, write a function that returns the number of set bits in its binary 
+representation (also known as the Hamming weight).
+
+Example 1:
+
+*/
+
 public class Q191NumberOfOneBits {
     
 }
