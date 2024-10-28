d75831519ba2212c09d8b2f5666ea58584451a40
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-10-28 19:20:55 +0530

    Added Q338CountingBits code

--- BitManupulation/Q338CountingBits.java
+++ BitManupulation/Q338CountingBits.java
@@ -1,5 +1,13 @@
 package BitManupulation;
 
+/*
+Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] 
+is the number of 1's in the binary representation of i.
+
+Example 1:
+
+*/
+
 public class Q338CountingBits {
     
 }
