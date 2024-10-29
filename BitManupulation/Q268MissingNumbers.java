6e0f8d425017b3633c0f54a828fee0695a72149e
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-10-29 19:37:01 +0530

    Added Q268MissingNumbers code

--- BitManupulation/Q268MissingNumbers.java
+++ BitManupulation/Q268MissingNumbers.java
@@ -21,5 +21,33 @@ Explanation: n = 9 since there are 9 numbers, so all numbers are in the range [0
 */
 
 public class Q268MissingNumbers {
-    
+
+    public int missingNumber(int[] arr) {
+
+        int i = 0;
+        while (i < arr.length) {
+            int correct = arr[i];
+            if (arr[i] < arr.length && arr[i] != arr[correct]) {
+                swap(arr, i, correct);
+            } else {
+                i++;
+            }
+        }
+
+        // search for first missing number
+        for (int index = 0; index < arr.length; index++) {
+            if (arr[index] != index) {
+                return index;
+            }
+        }
+
+        // case 2
+        return arr.length;
+    }
+
+    void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
 }
