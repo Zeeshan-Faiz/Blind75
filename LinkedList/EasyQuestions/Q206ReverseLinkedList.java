91a2cc23ee38882122eeaa7acef64ceb5d6db417
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-10-31 16:03:01 +0530

    Added Q206ReverseLinkedList code

--- LinkedList/EasyQuestions/Q206ReverseLinkedList.java
+++ LinkedList/EasyQuestions/Q206ReverseLinkedList.java
@@ -36,4 +36,19 @@ public class Q206ReverseLinkedList {
         }
         return prev;
     }
+
+    public class ListNode {
+        
+        int val;
+        ListNode next;
+        ListNode() {
+        }
+        ListNode(int val) {
+            this.val = val;
+        }
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
 }
