c2410b69569b6f0d76385908d0fcbb1534d86527
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-10-31 16:02:06 +0530

    Added Q206ReverseLinkedList code

--- LinkedList/EasyQuestions/Q206ReverseLinkedList.java
+++ LinkedList/EasyQuestions/Q206ReverseLinkedList.java
@@ -1,5 +1,12 @@
 package LinkedList.EasyQuestions;
 
+/*
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+Example 1:
+
+*/
+
 public class Q206ReverseLinkedList {
     
 }
