04ed6d4f117ffb6796330a76cb9d2e874caa5ef3
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-10-31 16:02:32 +0530

    Added Q206ReverseLinkedList code

--- LinkedList/EasyQuestions/Q206ReverseLinkedList.java
+++ LinkedList/EasyQuestions/Q206ReverseLinkedList.java
@@ -17,5 +17,23 @@ Output: []
 */
 
 public class Q206ReverseLinkedList {
-    
+
+    public ListNode reverseList(ListNode head) {
+
+        if (head == null)
+            return head;
+
+        ListNode prev = null;
+        ListNode present = head;
+        ListNode next = present.next;
+
+        while (present != null) {
+            present.next = prev;
+            prev = present;
+            present = next;
+            if (next != null)
+                next = next.next;
+        }
+        return prev;
+    }
 }
