c46eb0ff8e1406fad737fc24d3ba96f61e77e248
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-11-02 19:40:10 +0530

    Added Q21MergeTwoSortedLists code

--- LinkedList/EasyQuestions/Q21MergeTwoSortedLists.java
+++ LinkedList/EasyQuestions/Q21MergeTwoSortedLists.java
@@ -1,5 +1,14 @@
 package LinkedList.EasyQuestions;
 
+/*
+You are given the heads of two sorted linked lists list1 and list2.
+Merge the two lists into one sorted list. The list should be made by splicing together the nodes of 
+the first two lists. Return the head of the merged linked list.
+
+Example 1:
+
+*/
+
 public class Q21MergeTwoSortedLists {
     
 }
