7f4a9026735051f693adf480f7eff242bbb5f353
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-11-03 16:56:45 +0530

    Addded Q23MergeKSortedLists code

--- LinkedList/MediumQuestions/Q23MergeKSortedLists.java
+++ LinkedList/MediumQuestions/Q23MergeKSortedLists.java
@@ -58,4 +58,19 @@ public class Q23MergeKSortedLists {
         temp.next = l1 != null ? l1 : l2;
         return head.next;
     }
+
+    public class ListNode {
+        
+        int val;
+        ListNode next;
+        ListNode() {
+        }
+        ListNode(int val) {
+            this.val = val;
+        }
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
 }
