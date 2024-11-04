15de490d7a94bfde3f4ae30b0812be58ac013c6e
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-11-04 17:56:48 +0530

    Added Q19RemoveNthNode code

--- LinkedList/MediumQuestions/Q19RemoveNthNode.java
+++ LinkedList/MediumQuestions/Q19RemoveNthNode.java
@@ -1,5 +1,12 @@
 package LinkedList.MediumQuestions;
 
+/*
+Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+Example 1:
+
+*/
+
 public class Q19RemoveNthNode {
     
 }
