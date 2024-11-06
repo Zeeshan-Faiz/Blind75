c95640f2fb3f207f8c4aa5a581ba4f13edacbf77
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-11-06 18:57:24 +0530

    Added Q242ValidAnagram code

--- Strings/EasyQuestions/Q242ValidAnagram.java
+++ Strings/EasyQuestions/Q242ValidAnagram.java
@@ -4,7 +4,12 @@ package Strings.EasyQuestions;
 Given two strings s and t, return true if t is an anagram of s, and false otherwise.
 
 Example 1:
+Input: s = "anagram", t = "nagaram"
+Output: true
 
+Example 2:
+Input: s = "rat", t = "car"
+Output: false
 */
 
 public class Q242ValidAnagram {
