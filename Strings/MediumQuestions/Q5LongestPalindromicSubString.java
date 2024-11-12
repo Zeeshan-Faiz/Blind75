0280481b18e54c7964519d5c0e89146a4637a769
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-11-12 18:19:12 +0530

    Added Q5LongestPalindromicSubString code

--- Strings/MediumQuestions/Q5LongestPalindromicSubString.java
+++ Strings/MediumQuestions/Q5LongestPalindromicSubString.java
@@ -15,4 +15,35 @@ Output: "bb"
 
 public class Q5LongestPalindromicSubString {
     
+    int maxLen = 0;// length of longest palindrome
+    int lo = 0;
+
+    public String longestPalindrome(String s) {
+
+        if (s.length() < 2)
+            return s;
+
+        char[] input = s.toCharArray();
+
+        // i act as a centre of palindrome
+        for (int i = 0; i < input.length; i++) {
+            expandPalindrome(input, i, i);//check for odd length palindrome
+            expandPalindrome(input, i, i + 1);//check for even length palindrome
+        }
+        return s.substring(lo, lo + maxLen);
+    }
+
+    public void expandPalindrome(char[] s, int j, int k) {
+        
+        while (j >= 0 && k < s.length && s[j] == s[k]) {
+            j--;// expand left
+            k++;// expand right
+        }
+
+        //resize the starting and end index of palindrome
+        if (maxLen < k - j - 1) {
+            maxLen = k - j - 1;
+            lo = j + 1;
+        }
+    }
 }
