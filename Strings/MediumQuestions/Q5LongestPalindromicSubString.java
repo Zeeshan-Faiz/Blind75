786a210e649160257c00511512e69645298544f2
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-11-12 18:18:58 +0530

    Added Q5LongestPalindromicSubString code

--- Strings/MediumQuestions/Q5LongestPalindromicSubString.java
+++ Strings/MediumQuestions/Q5LongestPalindromicSubString.java
@@ -4,7 +4,13 @@ package Strings.MediumQuestions;
 Given a string s, return the longest palindromic substring in s.
 
 Example 1:
+Input: s = "babad"
+Output: "bab"
+Explanation: "aba" is also a valid answer.
 
+Example 2:
+Input: s = "cbbd"
+Output: "bb"
 */
 
 public class Q5LongestPalindromicSubString {
