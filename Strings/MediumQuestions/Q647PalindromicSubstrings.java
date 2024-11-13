e5d4dbe98acde21979d69423a17c5270d580a291
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-11-13 18:44:31 +0530

    Added Q647PalindromicSubstrings code

--- Strings/MediumQuestions/Q647PalindromicSubstrings.java
+++ Strings/MediumQuestions/Q647PalindromicSubstrings.java
@@ -17,4 +17,23 @@ Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
 
 public class Q647PalindromicSubstrings {
     
+    public int countSubstrings(String s) {
+        
+        int n = s.length();
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            int even = palindromeCount(s, i, i + 1);
+            int odd = palindromeCount(s, i - 1, i + 1);
+            ans += even + odd + 1;
+        }
+        return ans;
+    }
+
+    public int palindromeCount(String s, int left, int right) {
+        int count = 0;
+        while (left >= 0 && right < s.length() && s.charAt(left--) == s.charAt(right++)) {
+            count++;
+        }
+        return count;
+    }
 }
