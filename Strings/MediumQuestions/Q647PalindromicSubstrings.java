064ed07f9cfca5cc364c62d4efbc23f1e62f8dd6
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-11-13 18:44:11 +0530

    Added Q647PalindromicSubstrings code

--- Strings/MediumQuestions/Q647PalindromicSubstrings.java
+++ Strings/MediumQuestions/Q647PalindromicSubstrings.java
@@ -1,5 +1,13 @@
 package Strings.MediumQuestions;
 
+/*
+Given a string s, return the number of palindromic substrings in it. A string is a palindrome when 
+it reads the same backward as forward. A substring is a contiguous sequence of characters within the string.
+
+Example 1:
+
+*/
+
 public class Q647PalindromicSubstrings {
     
 }
