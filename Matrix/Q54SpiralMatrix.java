b04b2de6c8d90b661c8ddf2fb51b62225e1d15c1
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-11-15 18:30:04 +0530

    Added Q54SpiralMatrix code

--- Matrix/Q54SpiralMatrix.java
+++ Matrix/Q54SpiralMatrix.java
@@ -1,5 +1,7 @@
 package Matrix;
 
+import java.util.*;
+
 /*
 Given an m x n matrix, return all elements of the matrix in spiral order.
 
@@ -13,5 +15,48 @@ Output: [1,2,3,4,8,12,11,10,9,5,6,7]
 */
 
 public class Q54SpiralMatrix {
-    
+
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> res = new ArrayList<>();
+
+        if (matrix.length == 0) {
+            return res;
+        }
+
+        int rowBegin = 0;
+        int rowEnd = matrix.length - 1;
+        int colBegin = 0;
+        int colEnd = matrix[0].length - 1;
+
+        while (rowBegin <= rowEnd && colBegin <= colEnd) {
+            // Traverse Right
+            for (int j = colBegin; j <= colEnd; j++) {
+                res.add(matrix[rowBegin][j]);
+            }
+            rowBegin++;
+
+            // Traverse Down
+            for (int j = rowBegin; j <= rowEnd; j++) {
+                res.add(matrix[j][colEnd]);
+            }
+            colEnd--;
+
+            if (rowBegin <= rowEnd) {
+                // Traverse Left
+                for (int j = colEnd; j >= colBegin; j--) {
+                    res.add(matrix[rowEnd][j]);
+                }
+            }
+            rowEnd--;
+
+            if (colBegin <= colEnd) {
+                // Traverse Up
+                for (int j = rowEnd; j >= rowBegin; j--) {
+                    res.add(matrix[j][colBegin]);
+                }
+            }
+            colBegin++;
+        }
+        return res;
+    }
 }
