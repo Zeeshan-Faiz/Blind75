a88ac1877871fa5b6d054b163b22b3f6f265e5e2
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-11-16 20:15:14 +0530

    Added Q48RotateImage code

--- Matrix/Q48RotateImage.java
+++ Matrix/Q48RotateImage.java
@@ -1,5 +1,14 @@
 package Matrix;
 
+/*
+You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. 
+DO NOT allocate another 2D matrix and do the rotation.
+
+Example 1:
+
+*/
+
 public class Q48RotateImage {
     
 }
