7bb3453165fc1533ea186771b078be77b9104469
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-11-17 15:12:40 +0530

    Added Q79WordSearch code

--- Matrix/Q79WordSearch.java
+++ Matrix/Q79WordSearch.java
@@ -6,7 +6,16 @@ The word can be constructed from letters of sequentially adjacent cells, where a
 horizontally or vertically neighboring. The same letter cell may not be used more than once.
 
 Example 1:
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+Output: true
 
+Example 2:
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "SEE"
+Output: true
+
+Example 3:
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCB"
+Output: false
 */
 
 public class Q79WordSearch {
