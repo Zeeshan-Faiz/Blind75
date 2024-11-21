caefc19fdf826a175d79a6bbc2a3adc83b28a782
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-11-21 10:46:28 +0530

    Added Q435NonOverlappingIntervals code

--- Interval/Q435NonOverlappingIntervals.java
+++ Interval/Q435NonOverlappingIntervals.java
@@ -1,5 +1,7 @@
 package Interval;
 
+import java.util.*;
+
 /*
 Given an array of intervals intervals where intervals[i] = [starti, endi], return the minimum 
 number of intervals you need to remove to make the rest of the intervals non-overlapping.
@@ -24,4 +26,20 @@ Explanation: You don't need to remove any of the intervals since they're already
 
 public class Q435NonOverlappingIntervals {
     
+    public int eraseOverlapIntervals(int[][] intervals) {
+        
+        // sort the interval wrt to end points
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[1], b[1]));
+
+        int n = intervals.length;
+        int prevEndTime = intervals[0][1],count = 1;
+
+        for (int i = 1; i < n; i++) {
+            if (intervals[i][0] >= prevEndTime) {
+                prevEndTime = intervals[i][1];
+                count++;
+            }
+        }
+        return n - count;
+    }
 }
