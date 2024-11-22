68995814317c75be4bb8e27b157449af9eebf84c
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-11-22 17:07:18 +0530

    Added Q70ClimbingStairs code

--- DynamicProgramming/Q70ClimbingStairs.java
+++ DynamicProgramming/Q70ClimbingStairs.java
@@ -21,5 +21,20 @@ Explanation: There are three ways to climb to the top.
 */
 
 public class Q70ClimbingStairs {
-    
+
+    public int climbStairs(int n) {
+
+        if (n == 1)
+            return 1;
+
+        int[] dp = new int[n + 1];
+        dp[1] = 1; // for step 1
+        dp[2] = 2; // for step 2
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
 }
