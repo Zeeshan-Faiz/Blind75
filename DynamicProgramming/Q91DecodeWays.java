6ec51c43285426e00ee6b531dbd70f22f8e407db
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-12-02 17:50:39 +0530

    Added Q91DecodeWays code

--- DynamicProgramming/Q91DecodeWays.java
+++ DynamicProgramming/Q91DecodeWays.java
@@ -22,6 +22,24 @@ The test cases are generated so that the answer fits in a 32-bit integer.
 
 Example 1:
 
+Input: s = "12"
+Output: 2
+Explanation:
+"12" could be decoded as "AB" (1 2) or "L" (12).
+
+Example 2:
+
+Input: s = "226"
+Output: 3
+Explanation:
+"226" could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2 6).
+
+Example 3:
+
+Input: s = "06"
+Output: 0
+Explanation:
+"06" cannot be mapped to "F" because of the leading zero ("6" is different from "06"). In this case, the string is not a valid encoding, so return 0.
 */
 
 public class Q91DecodeWays {
