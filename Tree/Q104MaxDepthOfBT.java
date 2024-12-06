8b0a7c5a96e6819c54ff25bfb4b4200fe8425f74
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-12-06 14:30:32 +0530

    Added Q104MaxDepthOfBT code

--- Tree/Q104MaxDepthOfBT.java
+++ Tree/Q104MaxDepthOfBT.java
@@ -1,5 +1,7 @@
 package Tree;
 
+import javax.swing.tree.TreeNode;
+
 /*
 Given the root of a binary tree, return its maximum depth.
 A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
@@ -15,4 +17,11 @@ Output: 2
 
 public class Q104MaxDepthOfBT {
     
+    public int maxDepth(TreeNode root) {
+
+        if (root == null)
+            return 0;
+
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
 }
