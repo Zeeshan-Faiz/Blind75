4124d367d65d47825629ed03bc2443a4e9ecf534
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-12-08 12:35:11 +0530

    Added Q100SameTree code

--- Tree/Q100SameTree.java
+++ Tree/Q100SameTree.java
@@ -20,4 +20,16 @@ Output: false
 
 public class Q100SameTree {
     
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        
+        if (p == null && q == null)
+            return true;
+        if (p == null || q == null)
+            return false;
+
+        else if (p.val == q.val)
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+
+        return false;
+    }
 }
