246da528d672aea05c0fac9256592b3f98ee8221
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-12-15 13:05:27 +0530

    Added Q226InvertBinaryTree code

--- Tree/Q226InvertBinaryTree.java
+++ Tree/Q226InvertBinaryTree.java
@@ -17,5 +17,20 @@ Output: []
 */
 
 public class Q226InvertBinaryTree {
-    
+
+    public TreeNode invertTree(TreeNode root) {
+        
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
 }
