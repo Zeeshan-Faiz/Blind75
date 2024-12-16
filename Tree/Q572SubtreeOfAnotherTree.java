32b7d300b214969c89ed392cbb60bfc815b13cd2
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-12-16 15:36:47 +0530

    Added Q572SubtreeOfAnotherTree code

--- Tree/Q572SubtreeOfAnotherTree.java
+++ Tree/Q572SubtreeOfAnotherTree.java
@@ -7,7 +7,12 @@ A subtree of a binary tree tree is a tree that consists of a node in tree and al
 descendants. The tree tree could also be considered as a subtree of itself.
 
 Example 1:
+Input: root = [3,4,5,1,2], subRoot = [4,1,2]
+Output: true
 
+Example 2:
+Input: root = [3,4,5,1,2,null,null,null,null,0], subRoot = [4,1,2]
+Output: false
 */
 
 public class Q572SubtreeOfAnotherTree {
