543a3ea249884f9561f373fd810c045bb147f2c2
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-12-17 12:29:48 +0530

    Add Q106ConstructBinaryTreeFromInAndPreorder code

--- Tree/Q106ConstructBinaryTreeFromInAndPreorder.java
+++ Tree/Q106ConstructBinaryTreeFromInAndPreorder.java
@@ -1,5 +1,13 @@
 package Tree;
 
+/*
+Given two integer arrays inorder and postorder where inorder is the inorder traversal of a binary 
+tree and postorder is the postorder traversal of the same tree, construct and return the binary tree.
+
+Example 1:
+
+*/
+
 public class Q106ConstructBinaryTreeFromInAndPreorder {
     
 }
