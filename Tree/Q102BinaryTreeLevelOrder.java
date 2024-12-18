9de9db2d8010e68167e7ca0c2da3aae5f94c809e
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-12-18 12:42:08 +0530

    Added Q102BinaryTreeLevelOrder code

--- Tree/Q102BinaryTreeLevelOrder.java
+++ Tree/Q102BinaryTreeLevelOrder.java
@@ -1,5 +1,10 @@
 package Tree;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 /*
 Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from 
 left to right, level by level).
@@ -18,5 +23,30 @@ Output: []
 */
 
 public class Q102BinaryTreeLevelOrder {
-    
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+
+        if (root == null)
+            return new ArrayList<>();
+
+        List<List<Integer>> result = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int len = queue.size();
+            List<Integer> temp = new ArrayList<>();
+            for (int i = 0; i < len; i++) {
+                TreeNode curr = queue.poll();
+                temp.add(curr.val);
+
+                if (curr.left != null)
+                    queue.add(curr.left);
+                if (curr.right != null)
+                    queue.add(curr.right);
+            }
+            result.add(temp);
+        }
+        return result;
+    }
 }
