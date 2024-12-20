d20ac57ed6256124817292bb508b1045f48ab85a
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-12-20 22:41:37 +0530

    Added Q230KthSmallestElementInBST code

--- Tree/Q230KthSmallestElementInBST.java
+++ Tree/Q230KthSmallestElementInBST.java
@@ -1,5 +1,13 @@
 package Tree;
 
+/*
+Given the root of a binary search tree, and an integer k, return the kth smallest value (1-indexed) 
+of all the values of the nodes in the tree.
+
+Example 1:
+
+*/
+
 public class Q230KthSmallestElementInBST {
     
 }
