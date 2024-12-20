348a8b477a69fd99ba6d56ab3b825dd34daf1b1b
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-12-20 22:42:25 +0530

    Added Q230KthSmallestElementInBST code

--- Tree/Q230KthSmallestElementInBST.java
+++ Tree/Q230KthSmallestElementInBST.java
@@ -38,4 +38,24 @@ public class Q230KthSmallestElementInBST {
 
         return helper(root.right, k);
     }
+
+    public class TreeNode {
+        
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
 }
