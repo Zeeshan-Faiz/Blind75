ee54e229041832102f8273b6fd161e257231a2a3
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-12-21 09:50:36 +0530

    Added Q235LowestCommonAncestor code

--- Tree/Q235LowestCommonAncestor.java
+++ Tree/Q235LowestCommonAncestor.java
@@ -1,5 +1,16 @@
 package Tree;
 
+/*
+Given a binary search tree (BST), find the lowest common ancestor (LCA) node of two given nodes in 
+the BST.
+According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two 
+nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to 
+be a descendant of itself).”
+
+Example 1:
+
+*/
+
 public class Q235LowestCommonAncestor {
     
 }
