cb9708e7e07a5909d40c707748988a36795bc319
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-12-25 17:48:07 +0530

    Added Q297SerializeDeserializeBinaryTree code

--- Tree/Q297SerializeDeserializeBinaryTree.java
+++ Tree/Q297SerializeDeserializeBinaryTree.java
@@ -47,4 +47,24 @@ public class Q297SerializeDeserializeBinaryTree {
         root.right = helper(queue);
         return root;
     }
+
+    public class TreeNode {
+        
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
 }
