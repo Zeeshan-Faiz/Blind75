65641e7497cd3bc86d0c43a582dccc747e514a19
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-12-26 13:04:27 +0530

    Added Q208ImplementTrie code

--- Trie/Q208ImplementTrie.java
+++ Trie/Q208ImplementTrie.java
@@ -14,7 +14,20 @@ Implement the Trie class:
     that has the prefix prefix, and false otherwise.
 
 Example 1:
+Input
+["Trie", "insert", "search", "search", "startsWith", "insert", "search"]
+[[], ["apple"], ["apple"], ["app"], ["app"], ["app"], ["app"]]
+Output
+[null, null, true, false, true, null, true]
 
+Explanation
+Trie trie = new Trie();
+trie.insert("apple");
+trie.search("apple");   // return True
+trie.search("app");     // return False
+trie.startsWith("app"); // return True
+trie.insert("app");
+trie.search("app");     // return True
 */
 
 public class Q208ImplementTrie {
