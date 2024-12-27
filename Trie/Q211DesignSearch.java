56e286d64320c8c1da344218039713ca35c94041
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-12-27 11:57:01 +0530

    Added Q211DesignSearch code

--- Trie/Q211DesignSearch.java
+++ Trie/Q211DesignSearch.java
@@ -12,7 +12,21 @@ Implement the WordDictionary class:
     false otherwise. word may contain dots '.' where dots can be matched with any letter.
 
 Example 1:
+Input
+["WordDictionary","addWord","addWord","addWord","search","search","search","search"]
+[[],["bad"],["dad"],["mad"],["pad"],["bad"],[".ad"],["b.."]]
+Output
+[null,null,null,null,false,true,true,true]
 
+Explanation
+WordDictionary wordDictionary = new WordDictionary();
+wordDictionary.addWord("bad");
+wordDictionary.addWord("dad");
+wordDictionary.addWord("mad");
+wordDictionary.search("pad"); // return False
+wordDictionary.search("bad"); // return True
+wordDictionary.search(".ad"); // return True
+wordDictionary.search("b.."); // return True
 */
 
 public class Q211DesignSearch {
