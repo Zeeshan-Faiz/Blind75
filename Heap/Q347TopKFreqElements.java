8f5dace1f98eb035b9e44800c536c53a919fc352
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-12-29 21:35:34 +0530

    Added Q347TopKFreqElements code

--- Heap/Q347TopKFreqElements.java
+++ Heap/Q347TopKFreqElements.java
@@ -1,5 +1,9 @@
 package Heap;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 /*
 Given an integer array nums and an integer k, return the k most frequent elements. You may return 
 the answer in any order.
@@ -14,5 +18,32 @@ Output: [1]
 */
 
 public class Q347TopKFreqElements {
-    
+
+    public int[] topKFrequent(int[] nums, int k) {
+        
+        @SuppressWarnings("unchecked")
+        List<Integer>[] bucket = new List[nums.length + 1];
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for (int num : nums) {
+            hm.put(num, hm.getOrDefault(num, 0) + 1);
+        }
+        for (int key : hm.keySet()) {
+            int freq = hm.get(key);
+            if (bucket[freq] == null) {
+                bucket[freq] = new ArrayList<>();
+            }
+            bucket[freq].add(key);
+        }
+        int[] ans = new int[k];
+        int pos = 0;
+        for (int i = bucket.length - 1; i >= 0; i--) {
+            if (bucket[i] != null) {
+                for (int j = 0; j < bucket[i].size() && pos < k; j++) {
+                    ans[pos] = bucket[i].get(j);
+                    pos++;
+                }
+            }
+        }
+        return ans;
+    }
 }
