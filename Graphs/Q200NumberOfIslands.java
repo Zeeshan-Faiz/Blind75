53e32b0bb295f48b485e54c38281856e629b1edb
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-12-31 23:50:54 +0530

    Added Q200NumberOfIslands code

--- Graphs/Q200NumberOfIslands.java
+++ Graphs/Q200NumberOfIslands.java
@@ -27,4 +27,29 @@ Output: 3
 
 public class Q200NumberOfIslands {
     
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                //found land, do dfs along itself 
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    count++;//once the recurse come back, increment count
+                }
+            }
+        }
+        return count;
+    }
+
+    private void dfs(char[][] grid, int i, int j) {
+
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0')
+            return;
+
+        grid[i][j] = '0';// mark '1' as '0' i.e visited
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
+    }
 }
