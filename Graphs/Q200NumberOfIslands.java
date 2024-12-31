93bf2305b5feb112107c49b953caa18499b46769
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-12-31 23:50:41 +0530

    Added Q200NumberOfIslands code

--- Graphs/Q200NumberOfIslands.java
+++ Graphs/Q200NumberOfIslands.java
@@ -7,7 +7,22 @@ An island is surrounded by water and is formed by connecting adjacent lands hori
 You may assume all four edges of the grid are all surrounded by water.
 
 Example 1:
+Input: grid = [
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]
+]
+Output: 1
 
+Example 2:
+Input: grid = [
+  ["1","1","0","0","0"],
+  ["1","1","0","0","0"],
+  ["0","0","1","0","0"],
+  ["0","0","0","1","1"]
+]
+Output: 3
 */
 
 public class Q200NumberOfIslands {
