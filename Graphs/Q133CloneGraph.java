c93c85fc31e5b3f729a1f4c7900cd887312af4b2
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2025-01-03 13:41:04 +0530

    Added Q133CloneGraph code

--- Graphs/Q133CloneGraph.java
+++ Graphs/Q133CloneGraph.java
@@ -20,7 +20,23 @@ The given node will always be the first node with val = 1. You must return the c
 node as a reference to the cloned graph.
 
 Exammple 1:
+Input: adjList = [[2,4],[1,3],[2,4],[1,3]]
+Output: [[2,4],[1,3],[2,4],[1,3]]
+Explanation: There are 4 nodes in the graph.
+1st node (val = 1)'s neighbors are 2nd node (val = 2) and 4th node (val = 4).
+2nd node (val = 2)'s neighbors are 1st node (val = 1) and 3rd node (val = 3).
+3rd node (val = 3)'s neighbors are 2nd node (val = 2) and 4th node (val = 4).
+4th node (val = 4)'s neighbors are 1st node (val = 1) and 3rd node (val = 3).
 
+Example 2:
+Input: adjList = [[]]
+Output: [[]]
+Explanation: Note that the input contains one empty list. The graph consists of only one node with val = 1 and it does not have any neighbors.
+
+Example 3:
+Input: adjList = []
+Output: []
+Explanation: This an empty graph, it does not have any nodes.
 */
 
 public class Q133CloneGraph {
