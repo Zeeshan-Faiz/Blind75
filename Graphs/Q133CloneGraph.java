28dee412fcb210263938e60f6ffdeccaed935d62
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2025-01-03 13:41:35 +0530

    Added Q133CloneGraph code

--- Graphs/Q133CloneGraph.java
+++ Graphs/Q133CloneGraph.java
@@ -1,9 +1,9 @@
 package Graphs;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-
-import org.w3c.dom.Node;
 
 /*
 Given a reference of a node in a connected undirected graph.
@@ -63,4 +63,21 @@ public class Q133CloneGraph {
         }
         return copyNode;
     }
+
+    class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
 }
